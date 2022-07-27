a211f498df5010005da8102c8080c2c1b3456178
Eduardo Virgilio <55003926+digitalhedu@users.noreply.github.com>
2022-07-27 16:28:42 -0300

    Update app.js

--- source/app.js
+++ source/app.js
@@ -23,9 +23,7 @@ app.use(session({
 })) 
 app.use(cookie())
 /* Middlewares Custom */
-app.use(require('./middlewares/user'))
-app.use(require('./middlewares/msg'))
-app.use(require('./middlewares/cart'))
+app.use(require('./middlewares/user')
 
 app.use(require("./routes/main.routes"))
 app.use('/products',require('./routes/products.routes'))
